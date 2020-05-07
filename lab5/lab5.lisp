5da8911e701b89c6b26f2ac0409c621b1efbb783
darthhater101 <razerfanat@gmail.com>
2020-05-07 18:25:09 +0300

    add complex func handle

--- lab5/lab5.lisp
+++ lab5/lab5.lisp
@@ -1,14 +1,14 @@
 (defun make-sum-or-sub (sign expr1 expr2)
   (cond
-    ((eq expr1 0) expr2)
+    ((eq expr1 0) (if (eq sign '-) (- expr2) expr2))
     ((eq expr2 0) expr1)
     (t (list sign expr1 expr2))))
 
 (defun make-prod (expr1 expr2)
   (cond
+    ((or (eq expr1 0) (eq expr2 0)) 0)
     ((eq expr1 1) expr2)
     ((eq expr2 1) expr1)
-    ((or (eq expr1 0) (eq expr2 0)) 0)
     (t (list '* expr1 expr2))))
 
 (defun make-div (expr1 expr2)
@@ -25,6 +25,17 @@
 
 (defun make-cos (expr)
   (list '- (list 'sin expr)))
+
+(defun diff-expt (var expr)
+  (let
+      ((arg1 (second expr))
+       (arg2 (third expr)))
+    (cond
+      ((numberp arg1) (make-prod (make-expt arg1 arg2) (make-log arg1)))
+      ((and (eq arg1 var) (numberp arg2)) (make-prod arg2 (make-expt arg1 (1- arg2))))
+      (t (make-prod
+	  (make-expt arg1 arg2)
+      	  (differentiate var (make-prod arg2 (make-log arg1))))))))
 
 (defun differentiate (var expr)
   (cond
@@ -49,9 +60,13 @@
 					      (make-prod (differentiate var arg1) arg2)
 					      (make-prod arg1 (differentiate var arg2)))
 			     (make-expt arg2 2)))
-	   ((eq 'expt operator) (make-prod arg2 (make-expt arg1 (1- arg2))))
-	   ((eq 'sin operator) (make-sin arg1))
-	   ((eq 'cos operator) (make-cos arg1)))))))
-		   
-		    
-    
+	   ((eq 'expt operator) (diff-expt var expr))
+	   ((eq 'sin operator)
+	    (make-prod
+	     (make-sin arg1)
+	     (differentiate var arg1)))
+	   ((eq 'cos operator)
+	    (make-prod
+	     (make-cos arg1)
+	     (differentiate var arg1)))
+	   ((eq 'log operator) (make-div 1 arg1)))))))
